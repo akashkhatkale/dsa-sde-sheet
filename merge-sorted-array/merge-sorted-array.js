eefd4db2cb221827818085302fde11644c6800ac
Akash Khatkale <40288769+akashkhatkale@users.noreply.github.com>
2021-12-16 13:16:54 +0530

    Add files via upload

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,20 @@
+var merge = function (nums1, m, nums2, n) {
+    var last = m + n - 1;
+
+    while (m > 0 && n > 0) {
+        if (nums2[n - 1] > nums1[m - 1]) {
+            nums1[last] = nums2[n - 1];
+            n -= 1
+        } else {
+            nums1[last] = nums1[m - 1];
+            m -= 1
+        }
+        last -= 1
+    }
+
+    while (n > 0) {
+        nums1[last] = nums2[n - 1];
+        n -= 1
+        last -= 1
+    }
+};
